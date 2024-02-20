8ead2a02e938002330cdbf99cd573f3e18cbd23d
eddielicaycay <eddielicaycay@gmail.com>
2024-02-20 11:44:07 -0800

    added feed template -Eddie

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,23 +1,14 @@
-<<<<<<< HEAD
 
-//import './App.css';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import './styles/App.css';
-=======
 import './components/styles/App.css';
->>>>>>> main
+// import Footer from './components/Footer';
+// import Navbar from './components/Navbar';
+// import Home from './components/Home';
+import Feed from './components/Feed';
 
 function App() {
   return (
-
-    <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <Home />
-      </header>
-      <Footer />
+    <div className='App'>
+      <Feed />
     </div>
   );
 }
