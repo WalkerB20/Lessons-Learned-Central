c084691e63c04c4c61595ef975091a883bdd3e1a
WalkerB20 <bradenwalk24@gmail.com>
2024-02-22 17:22:46 -0500

    established solid starting point for footer and about/contact page

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './components/Styles/App.css';
-// import Footer from './components/Footer';
+import Footer from './components/Footer/index.jsx';
 // import Navbar from './components/Navbar';
 // import Home from './components/Home';
 import AARComponent from './components/AARComponent/index.jsx';
 import NavbarLogin from './components/NavbarLogin/index.jsx';
 import Home from './components/Home/index.jsx';
+import About from './components/About/index.jsx';
 
 export default function App() {
   return (
@@ -15,7 +16,9 @@ export default function App() {
           <Routes>
             <Route path="/llc" element={<AARComponent />} />
             <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
           </Routes>
+          <Footer />
       </div>
     </Router>
   );
