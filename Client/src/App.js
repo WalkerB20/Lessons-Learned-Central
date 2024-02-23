188b1c8de5d3753cae6eac6a6547b9df12efb9de
WalkerB20 <bradenwalk24@gmail.com>
2024-02-23 16:28:06 -0500

    made some improvements to the about and contact page

--- Client/src/App.js
+++ Client/src/App.js
@@ -7,6 +7,7 @@ import AARComponent from './components/AARComponent/index.jsx';
 import NavbarLogin from './components/NavbarLogin/index.jsx';
 import Home from './components/Home/index.jsx';
 import About from './components/About/index.jsx';
+import Contact from './components/Contact/index.jsx';
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
             <Route path="/llc" element={<AARComponent />} />
             <Route exact path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
           <Footer />
       </div>
