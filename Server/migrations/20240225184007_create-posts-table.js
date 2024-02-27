6747af7a42f081e7c68e192e195a8001985d1d33
WalkerB20 <bradenwalk24@gmail.com>
2024-02-27 15:46:54 -0500

    further troubleshooting migrations

--- Server/migrations/20240225184007_create-posts-table.js
+++ Server/migrations/20240225184007_create-posts-table.js
@@ -7,7 +7,7 @@ export const up = function(knex) {
         table.increments('Post_ID').primary(); // Primary key for the Posts table.
         table.string('Title').notNullable(); // Title of the post.
         table.text('Content').notNullable(); // Content of the post, using text type for longer text.
-        table.integer('User_ID').unsigned().notNullable(); // Foreign key to the User table.
+        table.uuid('User_ID').unsigned().notNullable(); // Foreign key to the User table.
         table.foreign('User_ID').references('User.User_ID'); // Establishing the foreign key relationship.
         table.timestamp('Created_At').defaultTo(knex.fn.now()); // Timestamp for when the post is created.
         table.timestamp('Updated_At').defaultTo(knex.fn.now()); // Timestamp for when the post is updated.
