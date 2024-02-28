bc10d9f01e98fa5fa1132b30205d16169871da80
EstherRGB2 <epicke2@gmail.com>
2024-02-27 19:03:52 -0500

    commit before merge

--- Client/src/App.js
+++ Client/src/App.js
@@ -5,13 +5,13 @@ import AARComponent from './components/AARComponent/index.jsx';
 import Home from './components/Home/index.jsx';
 import About from './components/About/index.jsx';
 import Contact from './components/Contact/index.jsx';
-// import Navbar from './components/Navbar/index.jsx';
+import Navbar from './components/Navbar/index.jsx';
 
 export default function App() {
   return (
       <>
       <div className="App">
-          {/* <Navbar /> */}
+          <Navbar />
           <Routes>
             <Route path="/llc" element={<AARComponent />} />
             <Route exact path="/" element={<Home />} />
